1ff39b0b0f3b83d1c770b703d907922bf0d30359
Ryuei <ryuei.sasaki@gmail.com>
2023-07-24 12:48:56 +0900

    add passthrough entry

--- .eleventy.js
+++ .eleventy.js
@@ -130,6 +130,7 @@ module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy('src/assets/images/')
 	eleventyConfig.addPassthroughCopy('src/assets/svg/')
 	eleventyConfig.addPassthroughCopy('src/assets/video/')
+  eleventyConfig.addPassthroughCopy('CNAME')
 
 	/**
 	 * Set custom markdown library instance...
